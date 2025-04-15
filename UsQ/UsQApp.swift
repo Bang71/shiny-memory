f372652b9fdecfb58ec8d46f53cfc70570c9fc23
Bang71 <es.bang71@icloud.com>
2025-04-15 18:28:48 +0900

    init: create project
    Initialized a new Xcode project

---
+++ UsQ/UsQApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsQApp.swift
+//  UsQ
+//
+//  Created by 신병기 on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct UsQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UsQTests/UsQTests.swift
@@ -0,0 +1,17 @@
+//
+//  UsQTests.swift
+//  UsQTests
+//
+//  Created by 신병기 on 4/15/25.
+//
+
+import Testing
+@testable import UsQ
+
+struct UsQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
